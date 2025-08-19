f82d302ced2878eda499126732d185ef293e642e
Ahmed Elsayed <ahmedelsayed3157@gmail.com>
2025-08-19 22:44:56 +0300

    session 4

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Task = (props) => {
-    const { text, done } = props
+    const { text, id, toggleDone, done } = props;
+
+
     return (
         <li>
             <span>{text}</span>
 
-            {done ? <button>Delete</button> : <button>Done</button>}
+            <button onClick={() => toggleDone(id)}>{done ? "Delete" : "Done"} </button>
         </li>
     )
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,20 +1,65 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Task from './Task';
 
 import './todo-list.css'
 
+/*
+        { id: 1, title: 'create project', desc: "", done: true },
+        { id: 2, title: "task 2", desc: "", done: false }
+   
+*/
+
 const TodoList = () => {
-
-    const [tasks, setTasks] = useState([
-        { text: 'create project', done: true },
-        { text: "task 2", done: false }
-    ]);
-
+    const [tasks, setTasks] = useState([]);
     const [text, setText] = useState("");
+    const [desc, setDesc] = useState("");
+
+    useEffect(() => {
+
+        document.querySelector('title').innerText = "task manager"
+
+        setTasks([
+            { id: 1, title: 'create project', desc: "", done: true },
+            { id: 2, title: "task 2", desc: "", done: false }
+        ])
+
+    }, [])
+
+
+    useEffect(() => {
+        console.log("new Tasks list is ")
+        console.log(tasks)
+
+
+    }, [tasks, desc])
+
+
 
     const addTask = () => {
 
-        let newTasks = [...tasks, { text: text, done: false }]
+        if (!text || !desc) return
+
+        let newTasks = [...tasks,
+        {
+            id: tasks.length + 1, // uuid
+            title: text,
+            desc: desc,
+            done: false
+        }]
+
+        setTasks(newTasks)
+        setDesc("")
+        setText("")
+    }
+
+    const toggleDone = (id) => {
+        const newTasks = tasks.map((t) => {
+            if (t.id === id) {
+                return { ...t, done: !t.done }
+            }
+
+            return t
+        })
 
         setTasks(newTasks)
     }
@@ -26,15 +71,31 @@ const TodoList = () => {
 
     }
 
+    const updateDesc = (e) => {
+        let newValue = e.target.value
+
+        setDesc(newValue)
+
+    }
+
     return (
         <div className='todo-container'>
-            <div className='input-group '>
-                <input className='input' value={text} onChange={updateText} type="text" />
-                <button className='button' onClick={addTask}>Create Task</button>
+            <div>
+                <h3>
+                    “You have {tasks.length} tasks, {tasks.filter((task) => task.done == true).length} completed”
+                </h3>
             </div>
+            <div className='input-group '>
+                <input className='input' placeholder='title' value={text} onChange={updateText} type="text" />
+            </div>
+            <div className="input-group">
+
+                <input type="text" placeholder='description' className='input' value={desc} onChange={updateDesc} />
+            </div>
+            <button className='button' onClick={addTask}>Create Task</button>
 
             <ul>
-                {tasks.map((task, index) => <Task key={index} text={task.text} done={task.done} />)}
+                {tasks.map((task, index) => <Task key={index} text={task.title} id={task.id} done={task.done} toggleDone={toggleDone} />)}
             </ul>
         </div>
     )
